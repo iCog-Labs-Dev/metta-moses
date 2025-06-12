9aaed05c77423c6242465e9afa8f9c24b95d9215
CodersKin <nebiyuhiyabu@gmail.com>
2025-06-12 13:58:51 +0300

    Fixed unittest errors

--- utilities/python-helpers/test_python_helpers.py
+++ utilities/python-helpers/test_python_helpers.py
@@ -1,14 +1,25 @@
-import pytest
-from listMethods import exprToList, listToExpr
+import unittest
+from .listMethods import exprToList, listToExpr
 
-def test_expr_to_list():
-    assert exprToList("(A B C)") == "(Cons A (Cons B (Cons C Nil)))"
-    assert exprToList("((mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (B) (C) D E)") == \
-        "(Cons (mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (Cons (B) (Cons (C) (Cons D (Cons E Nil)))))"
-    assert exprToList("(A (B C E) D E)") == "(Cons (B C E) (Cons A (Cons D (Cons E Nil))))"
 
-def test_list_to_expr():
-    assert listToExpr("(Cons A (Cons B (Cons C Nil)))") == "(A B C)"
-    assert listToExpr("(Cons (mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (Cons (B) (Cons (C) (Cons D (Cons E Nil)))))") == \
-    "((mkInst (0 1 2)) (B) (C) D E)"
-    assert listToExpr("(Cons (B C E) (Cons A (Cons D (Cons E Nil))))") == "((B C E) A D E)"
+class TestListMethods(unittest.TestCase):
+    
+    def test_listToExpr(self):
+        expected_result_1 = "(A B C)"
+        received_result_1 = listToExpr("(Cons A (Cons B (Cons C Nil)))")
+        expected_result_2 = "((mkInst (0 1 2)) (B) (C) D E)"
+        received_result_2 = listToExpr("(Cons (mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (Cons (B) (Cons (C) (Cons D (Cons E Nil)))))")
+        self.assertEqual(received_result_1, expected_result_1)
+        self.assertEqual(received_result_2, expected_result_2)
+    
+    def test_exprToList(self):
+        expected_result_1 = "(Cons A (Cons B (Cons C Nil)))"
+        received_result_1 = exprToList("(A B C)")
+        expected_result_2 = "(Cons (mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (Cons (B) (Cons (C) (Cons D (Cons E Nil)))))"
+        received_result_2 = exprToList("((mkInst (Cons 0 (Cons 1 (Cons 2 Nil)))) (B) (C) D E)")
+        expected_result_3 = "(Cons (B C E) (Cons A (Cons D (Cons E Nil))))"
+        received_result_3 = exprToList("(A (B C E) D E)")
+        self.assertEqual(received_result_1, expected_result_1)
+        self.assertEqual(received_result_2, expected_result_2)
+        self.assertEqual(received_result_3, expected_result_3)
+    
--- utilities/python-treehelpers/test_python_treehelpers.py
+++ utilities/python-treehelpers/test_python_treehelpers.py
@@ -1,26 +1,54 @@
-import pytest
-from treeMethods import getById, appendChild, insertAbove, replaceNode
+import unittest
+from .treeMethods import getById, appendChild, insertAbove, replaceNode
 
-tree = """(mkTree (mkNode AND) 
-                (Cons (mkTree (mkNode A) Nil) 
-                (Cons (mkTree (mkNode B) Nil) 
-                (Cons (mkTree (mkNode OR) 
-                    (Cons (mkTree (mkNode AND) 
-                        (Cons (mkTree (mkNode C) Nil) Nil)) 
-                        (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"""
 
-def test_getById():
-    assert getById(tree, '3') == "(mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil)))"
-    assert getById(tree, '(3 1 1)') == "(mkTree (mkNode C) Nil)"
+class TestTreeMethods(unittest.TestCase):
+    
+    def setUp(self):
+        self.tree =  """(mkTree (mkNode AND) 
+                            (Cons (mkTree (mkNode A) Nil) 
+                            (Cons (mkTree (mkNode B) Nil) 
+                            (Cons (mkTree (mkNode OR) 
+                                (Cons (mkTree (mkNode AND) 
+                                    (Cons (mkTree (mkNode C) Nil) Nil)) 
+                                    (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"""
 
-def test_appendChild():
-    assert appendChild(tree, '3', '(mkTree (mkNode E) Nil)') == "((mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) (Cons (mkTree (mkNode E) Nil) Nil)))) Nil)))) (mkNodeId (3 3)))"
-    assert appendChild(tree, '0', '(mkNullVex Nil)') == "((mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) (Cons (mkNullVex Nil) Nil))))) (mkNodeId (4)))"
 
-def test_insertAbove():
-    assert insertAbove(tree, '(3 1)', '(mkTree (mkNode XOR) Nil)') == "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode XOR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
-    assert insertAbove(tree, '1', '(mkTree (mkNode NOT) Nil)') == "(mkTree (mkNode AND) (Cons (mkTree (mkNode NOT) (Cons (mkTree (mkNode A) Nil) Nil)) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+    def test_getByID(self):
+        expected_result_1 = "(mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil)))"
+        received_result_1 =  getById(self.tree, '3')
+        expected_result_2 = "(mkTree (mkNode C) Nil)"
+        received_result_2 = getById(self.tree, '(3 1 1)')
 
-def test_replaceNode():
-    assert replaceNode(tree, '3', '(mkTree (mkNode XOR) Nil)') == "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode XOR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
-    assert replaceNode(tree, '(3 1 1)', '(mkTree (mkNode E) Nil)') == "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode E) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+        self.assertEqual(expected_result_1, received_result_1)
+        self.assertEqual(expected_result_2, received_result_2)
+
+
+    def test_appendChild(self):
+        expected_result_1 = "((mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) (Cons (mkTree (mkNode E) Nil) Nil)))) Nil)))) (mkNodeId (3 3)))"
+        received_result_1 = appendChild(self.tree, '3', '(mkTree (mkNode E) Nil)')
+        expected_result_2 = "((mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) (Cons (mkNullVex Nil) Nil))))) (mkNodeId (4)))"        
+        received_result_2 = appendChild(self.tree, '0', '(mkNullVex Nil)')
+
+        self.assertEqual(expected_result_1, received_result_1)
+        self.assertEqual(expected_result_2, received_result_2)
+    
+    
+    def test_insertAbove(self):
+        expected_result_1 = "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode XOR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+        received_result_1 = insertAbove(self.tree, '(3 1)', '(mkTree (mkNode XOR) Nil)')
+        expected_result_2 = "(mkTree (mkNode AND) (Cons (mkTree (mkNode NOT) (Cons (mkTree (mkNode A) Nil) Nil)) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+        received_result_2 = insertAbove(self.tree, '1', '(mkTree (mkNode NOT) Nil)')
+
+        self.assertEqual(expected_result_1, received_result_1)
+        self.assertEqual(expected_result_2, received_result_2)
+    
+    
+    def test_replaceNode(self):
+        expected_result_1 = "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode XOR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode C) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+        received_result_1 = replaceNode(self.tree, '3', '(mkTree (mkNode XOR) Nil)')
+        received_result_2 = replaceNode(self.tree, '(3 1 1)', '(mkTree (mkNode E) Nil)')
+        expected_result_2 = "(mkTree (mkNode AND) (Cons (mkTree (mkNode A) Nil) (Cons (mkTree (mkNode B) Nil) (Cons (mkTree (mkNode OR) (Cons (mkTree (mkNode AND) (Cons (mkTree (mkNode E) Nil) Nil)) (Cons (mkTree (mkNode D) Nil) Nil))) Nil))))"
+
+        self.assertEqual(expected_result_1, received_result_1)
+        self.assertEqual(expected_result_2, received_result_2)
